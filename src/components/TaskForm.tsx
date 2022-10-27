88249a308efdd8e0d7be3efb4cca008bd561eb6d
RonLamp <lamp@rc12.net>
2022-10-27 14:20:45 -0300

    first commit

---
+++ src/components/TaskForm.tsx
@@ -0,0 +1,81 @@
+import React, { useState, ChangeEvent, FormEvent, useEffect } from "react";
+// import css
+import styles from "./TaskForm.module.css";
+// tarefas
+import { ITask } from "../interfaces/Task";
+interface Props {
+  btnText: string;
+  taskList: ITask[];
+  setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>;
+  task?: ITask | null;
+  handleUpdate?(id: number, title: string, difficulty: number): void;
+}
+
+const TaskForm = ({
+  btnText,
+  taskList,
+  setTaskList,
+  task,
+  handleUpdate,
+}: Props) => {
+  const [id, setId] = useState<number>(0);
+  const [title, setTitle] = useState<string>("");
+  const [difficulty, setDifficulty] = useState<number>(0);
+  useEffect(() => {
+    if (task) {
+      setId(task.id);
+      setTitle(task.title);
+      setDifficulty(task.difficulty);
+    }
+  }, [task]);
+
+  const handleAddTask = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (handleUpdate) {
+      handleUpdate(id, title, difficulty);
+    } else {
+      const id = Math.floor(Math.random() * 10000);
+      const newTask: ITask = { id, title, difficulty };
+      setTaskList!([...taskList, newTask]);
+      setTitle("");
+      setDifficulty(0);
+      //console.log(taskList);
+    }
+  };
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === "title") {
+      setTitle(e.target.value);
+    } else {
+      setDifficulty(parseInt(e.target.value));
+    }
+  };
+
+  return (
+    <form onSubmit={handleAddTask} className={styles.form}>
+      <div className={styles.input_container}>
+        <label htmlFor="title">Título: </label>
+        <input
+          type="text"
+          name="title"
+          placeholder="Título da tarefa"
+          onChange={handleChange}
+          value={title}
+        />
+      </div>
+      <div className={styles.input_container}>
+        <label htmlFor="difficulty">Dificuldade da tarefa (1 a 5): </label>
+        <input
+          type="number"
+          name="difficulty"
+          onChange={handleChange}
+          min="0"
+          max="5"
+          value={difficulty}
+        />
+      </div>
+      <input type="submit" value={btnText} />
+    </form>
+  );
+};
+
+export default TaskForm;
